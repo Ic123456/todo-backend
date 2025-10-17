01d44a3472a7f8c8709d977c2b50dd176c4f2979
Isreal Christopher <isreal2017b@gmail.com>
2025-10-17 09:50:59 +0100

    Fix app paths for Render

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "ToDo",
-    "Accounts",
+    "Core.ToDo",
+    "Core.Accounts",
     'rest_framework',
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders"
