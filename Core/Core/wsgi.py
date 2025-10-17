fb0dbd301cfe6dfa95082f339b74e9f096c98115
Isreal Christopher <isreal2017b@gmail.com>
2025-10-17 16:38:25 +0100

    fix wsgi errort

--- Core/Core/wsgi.py
+++ Core/Core/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add project root to Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Core.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
