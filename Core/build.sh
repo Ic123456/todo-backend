b3a9849f291cd8c109c677cd7302a006b469a6f6
Isreal Christopher <isreal2017b@gmail.com>
2025-10-17 11:54:23 +0100

    Fix utils import

--- Core/build.sh
+++ Core/build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python Core/manage.py collectstatic --no-input
+python Core/manage.py migrate
